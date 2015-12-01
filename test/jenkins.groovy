4a3d85007c2ff961433e5533cc24c9f7e797ffdb
Pedro Roque Marques <roque@juniper.net>
2015-12-01 11:36:48 -0800

    Use MultiBinding in order to specify environment variables for AWS credentials.

--- test/jenkins.groovy
+++ test/jenkins.groovy
@@ -11,7 +11,7 @@ def test_ec2_k8s_basic = {
     node {
         git url: 'https://github.com/Juniper/container-networking-ansible.git'
 
-        withCredentials([[$class: 'UsernamePasswordBinding', credentialsId: 'k8s-provisioner', 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY']]) {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'k8s-provisioner', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
             // create cluster
             sh("ansible-playbook -i localhost playbook.yml --tags=create -e job_id=$BUILD_TAG")
         }
