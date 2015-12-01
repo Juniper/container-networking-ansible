cf7b662dfde1f968e148fa1f646ae70f1749943d
Pedro Roque Marques <roque@juniper.net>
2015-12-01 11:44:18 -0800

    Correct syntax for withCredentials.

--- test/jenkins.groovy
+++ test/jenkins.groovy
@@ -16,18 +16,18 @@ def test_ec2_k8s_basic = {
             sh("ansible-playbook -i localhost playbook.yml --tags=create -e job_id=$BUILD_TAG")
         }
 
-        withCredentials([[$class: 'FileBinding', credentialsId: 'infra-ssh-key', 'SSH_PRIVATE_KEY']]) {
+        withCredentials([[$class: 'FileBinding', credentialsId: 'k8s-ssh-key', variable: 'SSH_PRIVATE_KEY']]) {
             sh("ansible-playbook -i localhost playbook.yml --private-key=$SSH_PRIVATE_KEY --tags=deployer-install")
-            sh("ansible-playbook -i localhost playbook.yml --private-key=$SSH_PRIVATE_KEY --tags=workspace")           
+            sh("ansible-playbook -i localhost playbook.yml --private-key=$SSH_PRIVATE_KEY --tags=workspace")
         }
-        {
+
+        # ssh steps
             // provision cluster
             // validate
             // example application
             // verify
-        }
 
-        withCredentials([[$class: 'UsernamePasswordBinding', credentialsId: 'k8s-provisioner', 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY']]) {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'k8s-provisioner', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
             // delete cluster
         }
     }
