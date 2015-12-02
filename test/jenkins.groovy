95866f38e6c7746c7e3fa3a4c8b3420c93d56026
Pedro Roque Marques <roque@juniper.net>
2015-12-01 20:45:27 -0800

    Change working directory for test.

--- test/jenkins.groovy
+++ test/jenkins.groovy
@@ -1,24 +1,26 @@
 test_ec2_k8s_basic = {
     node {
         // git url: 'https://github.com/Juniper/container-networking-ansible.git'
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'k8s-provisioner', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-            // create cluster
-            sh "ansible-playbook -i localhost playbook.yml --tags=create -e job_id=${env.BUILD_NUMBER}"
-        }
+        dir('test/ec2-k8s') {
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'k8s-provisioner', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                // create cluster
+                sh "ansible-playbook -i localhost playbook.yml --tags=create -e job_id=${env.BUILD_NUMBER}"
+            }
 
-        withCredentials([[$class: 'FileBinding', credentialsId: 'k8s.key', variable: 'SSH_PRIVATE_KEY']]) {
-            sh("ansible-playbook -i localhost playbook.yml --private-key=$SSH_PRIVATE_KEY --tags=deployer-install")
-            sh("ansible-playbook -i localhost playbook.yml --private-key=$SSH_PRIVATE_KEY --tags=workspace")
-        }
+            withCredentials([[$class: 'FileBinding', credentialsId: 'k8s.key', variable: 'SSH_PRIVATE_KEY']]) {
+                sh("ansible-playbook -i localhost playbook.yml --private-key=$SSH_PRIVATE_KEY --tags=deployer-install")
+                sh("ansible-playbook -i localhost playbook.yml --private-key=$SSH_PRIVATE_KEY --tags=workspace")
+            }
 
-        // ssh client steps:
-        // provision cluster
-        // validate
-        // example application
-        // verify
+            // ssh client steps:
+            // provision cluster
+            // validate
+            // example application
+            // verify
 
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'k8s-provisioner', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-            // delete cluster
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'k8s-provisioner', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                // delete cluster
+            }
         }
     }
 }
